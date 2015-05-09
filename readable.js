d6cb9a32ceebea5f1475843bb7a214477318bd95
oraclepxx <oraclepxx@gmail.com>
2015-05-09 10:07:50 -0700

    -update

--- readable.js
+++ readable.js
@@ -5,27 +5,51 @@
 var stream = require('stream');
 var util = require('util');
 
-util.inherits(Address, stream.Readable);
+//util.inherits(Reader, stream.Readable);
+//
+//function Reader(opt) {
+//
+//    stream.Readable.call(this, opt);
+//    this.data = "3303 Hillview Ave, Palo Alto, CA, 94304";
+//};
+//
+//Reader.prototype._read = function () {
+//    this.push(this.data);
+//};
+//
+//var reader = new Reader();
+//console.log(reader.read());
+//
+//reader.on('data', function (data) {
+//    console.log(data.toString());
+//});
+//
+//reader.on('end', function (data) {
+//    console.log("Done.  " + data);
+//});
 
-function Address(opt) {
 
-    stream.Readable.call(this, opt);
-    this.details = "3303 Hillview Ave, Palo Alto, CA, 94304";
+util.inherits(Writer, stream.Writable);
+
+function Writer(opt) {
+    stream.Writable.call(this);
+    this.data = new Array();
 };
 
-Address.prototype._read = function () {
-    this.push(this.details);
+Writer.prototype._write = function (chunk, encoding, callback) {
+    if (chunk != 'Done') {
+        this.data.push(chunk);
+    }
+    console.log('Writing data ' + chunk);
+    callback();
 };
 
-var addr = new Address();
-console.log(addr.read());
+var writer = new Writer();
+for (var i = 1; i <= 5; i++) {
+    var item = 'Item' + i;
+    writer.write(item, 'utf8');
+}
 
-addr.on('data', function (data) {
-    console.log(data.toString());
-});
-
-addr.on('end', function (data) {
-    console.log("Done.  " + data);
-});
-
+writer.end('Done.');
+console.log(writer.data);
 
